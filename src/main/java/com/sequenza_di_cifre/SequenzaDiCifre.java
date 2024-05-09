853d2354bbfcd1d70c231d32de35a0a9f4cb3097
Arianna Paolini <129447232+arianna011@users.noreply.github.com>
2024-05-09 09:51:37 +0200

    Update SequenzaDiCifre.java

--- src/main/java/com/sequenza_di_cifre/SequenzaDiCifre.java
+++ src/main/java/com/sequenza_di_cifre/SequenzaDiCifre.java
@@ -5,30 +5,48 @@ package com.sequenza_di_cifre;
  */
 public class SequenzaDiCifre 
 {
+	private char[] sequenza = new char[]{};
 	
-	//implementare campi della classe
-	
-	/**
-	 * Metodo che restituisce la sequenza di cifre 
-	 **/
-	public char[] getSequenza()
-	{ 
-		//implementare
-		return null; 
-	}
+	public char[] getSequenza() { return sequenza; }
 	
 	/**
 	 * Metodo che, data in input una stringa e un intero N, 
 	 * aggiunge alla sequenza le prime N cifre contenute nella stringa
-	 * (se la stringa contiene meno di N cifre, aggiungere quelle presenti).
-	 * Se N è un numero negativo, sollevare IllegalArgumentException con parametro N.
+	 * (se la stringa contiene meno di N cifre, aggiungere quelle presenti)
 	 */
 	public void aggiungiCifre(String s, int N) {
 		
-		//implementare
+		if (N < 0) throw new IllegalArgumentException(String.valueOf(N));
+		
+		ArrayList<Character> l = new ArrayList<Character>();
+		
+		for (char c : s.toCharArray()) {
+			if (Character.isDigit(c)) l.add(c);
+		}
+		
+		if (l.size() == 0) return;
+		
+		char[] newSeq = new char[sequenza.length + Math.min(N, l.size())];
+		
+		for (int i = 0; i<newSeq.length; i++) {
+			if (i<sequenza.length) newSeq[i] = sequenza[i];
+			else newSeq[i] = l.get(i - sequenza.length);
+		}
+		
+		sequenza = newSeq;
 	}
 	
-	
 	//sovrascrivere il metodo toString ereditato dalla classe Object
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("[");
+		for (int i=0; i<sequenza.length; i++) {
+			sb.append(sequenza[i]);
+			if (i != sequenza.length-1) sb.append(",");
+		}
+		sb.append("]");
+		return sb.toString();	
+	}
+	
 	
 }
